08689c7fe764f62aa768aaad1832b089ef7e484c
Josh Campbell <joshcb@amazon.com>
2016-09-30 10:41:07 -0700

    adds logging and fixes formatting

--- src/handlers/de-de.handlers.js
+++ src/handlers/de-de.handlers.js
@@ -1,26 +1,31 @@
 module.exports = {
   'LaunchRequest': function () {
+    console.log("LaunchRequest fired...");
     this.attributes.speechOutput = "Hallo, ich bin Josh bot. Was brauchst du?";
     this.attributes.repromptSpeech = "Man kann auch sagen, mir helfen.";
     this.emit(':ask', this.attributes.speechOutput, this.attributes.repromptSpeech);
   },
   'PhraseIntent': function () {
+    console.log("PhraseIntent fired...");
     var phraseIndex = Math.floor(Math.random() * deDePhrases.length);
-    var randomPhrase = deDePhrases[phraseIndex];
-
-    var speechOutput = randomPhrase;
+        randomPhrase = deDePhrases[phraseIndex];
+        speechOutput = randomPhrase;
+    console.log("phraseIndex is:  " + phraseIndex);
 
     this.emit(':tellWithCard', speechOutput, SKILL_NAME, randomPhrase);
   },
   'AMAZON.HelpIntent': function () {
-      var speechOutput = "Sie können mir eine Phrase sagen geben, oder, Ausfahrt sagen kann ... Was kann ich Ihnen helfen?";
-      var reprompt = "Womit kann ich dir helfen?";
-      this.emit(':ask', speechOutput, reprompt);
+    console.log("HelpIntent fired...");
+    var speechOutput = "Sie können mir eine Phrase sagen geben, oder, Ausfahrt sagen kann ... Was kann ich Ihnen helfen?";
+        reprompt = "Womit kann ich dir helfen?";
+    this.emit(':ask', speechOutput, reprompt);
   },
   'AMAZON.CancelIntent': function () {
-      this.emit(':tell', 'Tschüss!');
+    console.log("CancelIntent fired...");
+    this.emit(':tell', 'Tschüss!');
   },
   'AMAZON.StopIntent': function () {
-      this.emit(':tell', 'Tschüss!');
+    console.log("StopIntent fired...");
+    this.emit(':tell', 'Tschüss!');
   }
 };
