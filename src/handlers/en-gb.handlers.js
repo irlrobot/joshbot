10680162035018f9d080ce021a29afe343661c83
Josh Campbell <joshcb@amazon.com>
2016-09-30 10:41:31 -0700

    adds logging and fixes formatting

--- src/handlers/en-gb.handlers.js
+++ src/handlers/en-gb.handlers.js
@@ -1,26 +1,31 @@
 module.exports = {
   'LaunchRequest': function () {
+    console.log("LaunchRequest fired...");
     this.attributes.speechOutput = "Hello, I'm Josh bot.  What do you need?";
     this.attributes.repromptSpeech = "You can also say, help me.";
     this.emit(':ask', this.attributes.speechOutput, this.attributes.repromptSpeech);
   },
   'PhraseIntent': function () {
+    console.log("PhraseIntent fired...");
     var phraseIndex = Math.floor(Math.random() * enGbPhrases.length);
-    var randomPhrase = enGbPhrases[phraseIndex];
-
-    var speechOutput = randomPhrase;
+        randomPhrase = enGbPhrases[phraseIndex];
+        speechOutput = randomPhrase;
+    console.log("phraseIndex is:  " + phraseIndex);
 
     this.emit(':tellWithCard', speechOutput, SKILL_NAME, randomPhrase);
   },
   'AMAZON.HelpIntent': function () {
-      var speechOutput = "You can say give me a phrase, or, you can say exit... What can I help you with?";
-      var reprompt = "What can I help you with?";
-      this.emit(':ask', speechOutput, reprompt);
+    console.log("HelpIntent fired...");
+    var speechOutput = "You can say give me a phrase, or, you can say exit... What can I help you with?";
+        reprompt = "What can I help you with?";
+    this.emit(':ask', speechOutput, reprompt);
   },
   'AMAZON.CancelIntent': function () {
-      this.emit(':tell', 'Cheers!');
+    console.log("CancelIntent fired...");
+    this.emit(':tell', 'Cheers!');
   },
   'AMAZON.StopIntent': function () {
-      this.emit(':tell', 'Cheers!');
+    console.log("StopIntent fired...");
+    this.emit(':tell', 'Cheers!');
   }
 };
